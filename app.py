1a291f7043e6d21f21bd877942bd76ab8999845d
alvinrach <alvinracht@gmail.com>
2024-08-28 23:15:00 +0700

    Revert back without local save

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from beyondllm.embeddings import FineTuneEmbeddings
 import shutil
 import nltk
 
-nltk_data_dir = "./resources/nltk_data_dir/"
-if not os.path.exists(nltk_data_dir):
-    os.makedirs(nltk_data_dir, exist_ok=True)
-nltk.data.path.clear()
-nltk.data.path.append(nltk_data_dir)
-nltk.download("stopwords", download_dir=nltk_data_dir)
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('punkt_tab', download_dir=nltk_data_dir)
+# nltk_data_dir = "./resources/nltk_data_dir/"
+# if not os.path.exists(nltk_data_dir):
+#     os.makedirs(nltk_data_dir, exist_ok=True)
+# nltk.data.path.clear()
+# nltk.data.path.append(nltk_data_dir)
+# nltk.download("stopwords", download_dir=nltk_data_dir)
+# nltk.download('punkt', download_dir=nltk_data_dir)
+# nltk.download('punkt_tab', download_dir=nltk_data_dir)
 
 st.title('Talk With Your CV!')
 st.markdown("""
